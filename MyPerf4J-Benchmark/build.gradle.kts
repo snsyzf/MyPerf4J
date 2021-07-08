2dd6f10b89f0fe070d7d2a6d4c11b2df5a10bee6
snsyzf <snsyzf@qq.com>
2021-07-08 23:01:31 +0800

    fix: 修复依赖问题

--- MyPerf4J-Benchmark/build.gradle.kts
+++ MyPerf4J-Benchmark/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 dependencies {
     implementation(project(":MyPerf4J-ASM"))
     implementation(project(":MyPerf4J-Base"))
+    implementation(project(":MyPerf4J-Core"))
     implementation("org.openjdk.jmh:jmh-core:1.23")
     implementation("org.openjdk.jmh:jmh-generator-annprocess:1.23")
     testImplementation("junit:junit:4.13.2")
